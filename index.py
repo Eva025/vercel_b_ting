3791aacde125d5e7b4f91cc49abd47f2143cc9d7
Eva025 <94.05.25jj@gmail.com>
2025-03-27 10:12:54 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>張綺庭Python網頁+8</h1>"
+    homepage = "<h1>張綺庭Python網頁+8a</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=ting>傳送使用者暱稱</a><br>"
@@ -31,6 +31,7 @@ def me():
 @app.route("/welcome", methods=["GET"])
 def welcome():
     user = request.values.get("nick")
+    w = request.values.get("work")
     return render_template("welcome.html", name=user , work=w)
 
 @app.route("/account", methods=["GET", "POST"])
